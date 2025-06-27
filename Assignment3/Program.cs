64fc9aa740107d7a44ebef67721b25ff9c266890
moneka <monekawilson16@gmail.com>
2025-06-27 14:39:46 +0300

    Q1

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -6,7 +6,30 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Q1
+            /*
+             Passing by Value means sending a copy of the variable to the method. Changes inside the method do not affect the original variable.
+             Passing by Reference means sending the actual memory reference of the variable. Changes inside the method do affect the original variable.
+             */
+            int number = 50;
+            ChangeValue(number);
+            Console.WriteLine("By Value → number = " + number);   //50
+
+            ChangeValue(ref number);
+            Console.WriteLine("By Reference → number = " + number);  //100
+            #endregion
         }
+
+        static void ChangeValue (int num)
+        {
+            num = 100;
+
+        }
+        static void ChangeValue(ref int num)
+    {
+        num = 100;
+    }
+
+
     }
 }
