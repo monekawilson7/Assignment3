ff0d9e9c5950543eb6f65e3ed6c45bae8b8d4358
moneka <monekawilson16@gmail.com>
2025-06-29 00:08:21 +0300

    Q5

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -5,29 +5,27 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
-
             #region Q1
 
-            Passing by Value means sending a copy of the variable to the method.Changes inside the method do not affect the original variable.
-              Passing by Reference means sending the actual memory reference of the variable. Changes inside the method do affect the original variable.
+            /*Passing by Value means sending a copy of the variable to the method.Changes inside the method do not affect the original variable.
+               Passing by Reference means sending the actual memory reference of the variable. Changes inside the method do affect the original variable.
+            */
 
-
-
-             int number = 50;
+              int number = 50;
             ChangeValue(number);
             Console.WriteLine("By Value → number = " + number);   //50
 
             ChangeValue(ref number);
             Console.WriteLine("By Reference → number = " + number);  //100
-
             #endregion
 
             #region Q2
-
+            /*
             Passing by value passes a copy of the reference.Changes to the object affect the original, but changing the reference itself does not.
-              Passing by reference allows the method to change both the object and the reference itself.
+               Passing by reference allows the method to change both the object and the reference itself.
+            */
 
-             Person person = new Person();
+              Person person = new Person();
             person.Name = "Ahmed";
             ChangePerson(person);
             Console.WriteLine(person.Name); //Ali
@@ -48,6 +46,22 @@ namespace Assignment3
             var result = Calculater(num1, num2, num3, num4);
             Console.WriteLine($"Sum of first two numbers = {result.sum}");
             Console.WriteLine($"Subtract of last two numbers = {result.sub}");
+            #endregion
+
+            #region Q4
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            int result = SumDigits(num);
+            Console.WriteLine(result);
+            #endregion
+
+            #region Q5
+            Console.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+            if (IsPrime(num))
+                Console.WriteLine($"{num} is prime number");
+            else
+                Console.WriteLine($"{num} is not prime number");
             #endregion
         }
 
@@ -83,13 +97,35 @@ namespace Assignment3
         #endregion
         #region Q3
         static (int sum, int sub) Calculater(int num1, int num2, int num3, int num4)
-            {
-            int sum = num1+ num2;
+        {
+            int sum = num1 + num2;
             int sub = num3 - num4;
-        return (sum,sub);
+            return (sum, sub);
         }
         #endregion
-
-
+        #region Q4
+        static int SumDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+            return sum;
+        }
+        #endregion
+        #region Q5
+        static bool IsPrime(int num) { 
+        if (num<=1)
+                return false;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        #endregion
     }
 }
