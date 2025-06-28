1d6f3d571a5fa8abfe495ad75b0576a1870e748a
moneka <monekawilson16@gmail.com>
2025-06-28 18:06:45 +0300

    Q2

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,25 +1,45 @@
 ﻿using System;
-
 namespace Assignment3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             #region Q1
             /*
              Passing by Value means sending a copy of the variable to the method. Changes inside the method do not affect the original variable.
              Passing by Reference means sending the actual memory reference of the variable. Changes inside the method do affect the original variable.
              */
+
             int number = 50;
             ChangeValue(number);
             Console.WriteLine("By Value → number = " + number);   //50
 
             ChangeValue(ref number);
             Console.WriteLine("By Reference → number = " + number);  //100
+
+            #endregion
+
+            #region Q2
+            /*
+             Passing by value passes a copy of the reference. Changes to the object affect the original, but changing the reference itself does not.
+             Passing by reference allows the method to change both the object and the reference itself.
+             */
+            Person person = new Person();
+            person.Name = "Ahmed";
+            ChangePerson(person);
+            Console.WriteLine(person.Name); //Ali
+
+
+            Person person = new Person();
+            person.Name = "Ahmed";
+            ChangePerson(ref person);
+            Console.WriteLine(person.Name); //Omar
             #endregion
         }
 
+        #region Q1 Functions
         static void ChangeValue (int num)
         {
             num = 100;
@@ -29,7 +49,26 @@ namespace Assignment3
     {
         num = 100;
     }
+        #endregion
+        #region Q2 Functions
 
+        static void ChangePerson(Person p)
+        {
+            p.Name = "Ali";
+            p = new Person();
+            p.Name = "Omar";
+        }
+
+        static void ChangePerson(ref Person p)
+        {
+            p = new Person();
+            p.Name = "Omar";
+        }
+        #endregion
+        class Person
+        {
+            public string Name;
+        }
 
     }
 }
